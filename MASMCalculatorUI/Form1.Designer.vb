aa734bef4187b0f1423977b66bcd41f79e022b36
BBQBaconBurger <czariman06@gmail.com>
2025-04-10 09:14:22 +0800

    Add files via upload
    This calculator uses assembly to compute the numbers and handles interface using VB.NET Frameworks

---
+++ MASMCalculatorUI/Form1.Designer.vb
@@ -0,0 +1,182 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.cmbOperation = New System.Windows.Forms.ComboBox()
+        Me.txtNum1 = New System.Windows.Forms.TextBox()
+        Me.txtNum2 = New System.Windows.Forms.TextBox()
+        Me.lblResult = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.lblNum1 = New System.Windows.Forms.Label()
+        Me.lblNum2 = New System.Windows.Forms.Label()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'cmbOperation
+        '
+        Me.cmbOperation.BackColor = System.Drawing.Color.Black
+        Me.cmbOperation.ForeColor = System.Drawing.Color.White
+        Me.cmbOperation.FormattingEnabled = True
+        Me.cmbOperation.Location = New System.Drawing.Point(349, 107)
+        Me.cmbOperation.Name = "cmbOperation"
+        Me.cmbOperation.Size = New System.Drawing.Size(121, 24)
+        Me.cmbOperation.TabIndex = 0
+        '
+        'txtNum1
+        '
+        Me.txtNum1.BackColor = System.Drawing.Color.Black
+        Me.txtNum1.ForeColor = System.Drawing.Color.White
+        Me.txtNum1.Location = New System.Drawing.Point(409, 156)
+        Me.txtNum1.Name = "txtNum1"
+        Me.txtNum1.Size = New System.Drawing.Size(154, 22)
+        Me.txtNum1.TabIndex = 1
+        '
+        'txtNum2
+        '
+        Me.txtNum2.BackColor = System.Drawing.Color.Black
+        Me.txtNum2.ForeColor = System.Drawing.Color.White
+        Me.txtNum2.Location = New System.Drawing.Point(409, 195)
+        Me.txtNum2.Name = "txtNum2"
+        Me.txtNum2.Size = New System.Drawing.Size(154, 22)
+        Me.txtNum2.TabIndex = 2
+        '
+        'lblResult
+        '
+        Me.lblResult.BackColor = System.Drawing.Color.Black
+        Me.lblResult.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblResult.ForeColor = System.Drawing.Color.White
+        Me.lblResult.ImageAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.lblResult.Location = New System.Drawing.Point(346, 363)
+        Me.lblResult.Name = "lblResult"
+        Me.lblResult.Size = New System.Drawing.Size(212, 29)
+        Me.lblResult.TabIndex = 3
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.BackColor = System.Drawing.Color.Black
+        Me.btnCalculate.Font = New System.Drawing.Font("Microsoft Sans Serif", 7.8!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCalculate.ForeColor = System.Drawing.Color.White
+        Me.btnCalculate.Location = New System.Drawing.Point(349, 298)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(102, 32)
+        Me.btnCalculate.TabIndex = 4
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = False
+        '
+        'lblNum1
+        '
+        Me.lblNum1.AutoSize = True
+        Me.lblNum1.BackColor = System.Drawing.Color.Black
+        Me.lblNum1.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNum1.ForeColor = System.Drawing.Color.White
+        Me.lblNum1.Location = New System.Drawing.Point(261, 156)
+        Me.lblNum1.Name = "lblNum1"
+        Me.lblNum1.Size = New System.Drawing.Size(125, 20)
+        Me.lblNum1.TabIndex = 5
+        Me.lblNum1.Text = "First Number:"
+        '
+        'lblNum2
+        '
+        Me.lblNum2.AutoSize = True
+        Me.lblNum2.BackColor = System.Drawing.Color.Black
+        Me.lblNum2.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblNum2.ForeColor = System.Drawing.Color.White
+        Me.lblNum2.Location = New System.Drawing.Point(238, 197)
+        Me.lblNum2.Name = "lblNum2"
+        Me.lblNum2.Size = New System.Drawing.Size(148, 20)
+        Me.lblNum2.TabIndex = 6
+        Me.lblNum2.Text = "Second Number:"
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.BackColor = System.Drawing.Color.Black
+        Me.lblTitle.Font = New System.Drawing.Font("Perpetua Titling MT", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.ForeColor = System.Drawing.Color.White
+        Me.lblTitle.Location = New System.Drawing.Point(246, 31)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(317, 48)
+        Me.lblTitle.TabIndex = 7
+        Me.lblTitle.Text = "Calculator"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackgroundImage = CType(resources.GetObject("PictureBox1.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 1)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(184, 468)
+        Me.PictureBox1.TabIndex = 8
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BackgroundImage = CType(resources.GetObject("PictureBox2.BackgroundImage"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(615, 1)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(184, 468)
+        Me.PictureBox2.TabIndex = 9
+        Me.PictureBox2.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.Black
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Controls.Add(Me.lblNum2)
+        Me.Controls.Add(Me.lblNum1)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.lblResult)
+        Me.Controls.Add(Me.txtNum2)
+        Me.Controls.Add(Me.txtNum1)
+        Me.Controls.Add(Me.cmbOperation)
+        Me.ForeColor = System.Drawing.Color.White
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cmbOperation As ComboBox
+    Friend WithEvents txtNum1 As TextBox
+    Friend WithEvents txtNum2 As TextBox
+    Friend WithEvents lblResult As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents lblNum1 As Label
+    Friend WithEvents lblNum2 As Label
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+End Class
---
+++ MASMCalculatorUI/Form1.vb
@@ -0,0 +1,95 @@
+﻿Imports System.Collections.Generic
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        cmbOperation.Items.Clear()
+        cmbOperation.Items.AddRange({
+            "Addition",        ' Index 0
+            "Subtraction",     ' Index 1
+            "Multiplication", ' Index 2
+            "Division",       ' Index 3
+            "Negation",       ' Index 4
+            "Square",         ' Index 5
+            "Cube",           ' Index 6
+            "OR",            ' Index 7
+            "AND",           ' Index 8
+            "XOR",           ' Index 9
+            "NOT",           ' Index 10
+            "Modulus"        ' Index 11
+        })
+        cmbOperation.SelectedIndex = 0
+    End Sub
+
+    Private Sub cmbOperation_SelectedIndexChanged(sender As Object, e As EventArgs) Handles cmbOperation.SelectedIndexChanged
+        Dim singleNumberOperations As New List(Of Integer) From {4, 5, 6, 10}
+
+        Dim showSecondNum = Not singleNumberOperations.Contains(cmbOperation.SelectedIndex)
+        txtNum2.Visible = showSecondNum
+        lblNum2.Visible = showSecondNum
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+        If Not Integer.TryParse(txtNum1.Text, Nothing) Then
+            MessageBox.Show("Please enter a valid first number")
+            Return
+        End If
+
+        Dim num1 As Integer = Integer.Parse(txtNum1.Text)
+        Dim num2 As Integer = 0
+        Dim result As Integer = 0
+
+        If cmbOperation.SelectedIndex <> 4 AndAlso ' Negation
+           cmbOperation.SelectedIndex <> 5 AndAlso ' Square
+           cmbOperation.SelectedIndex <> 6 AndAlso ' Cube
+           cmbOperation.SelectedIndex <> 10 Then  ' NOT
+            If Not Integer.TryParse(txtNum2.Text, Nothing) Then
+                MessageBox.Show("Please enter a valid second number")
+                Return
+            End If
+            num2 = Integer.Parse(txtNum2.Text)
+        End If
+
+        Select Case cmbOperation.SelectedIndex
+            Case 0 : result = num1 + num2      ' Addition
+            Case 1 : result = num1 - num2      ' Subtraction
+            Case 2 : result = num1 * num2      ' Multiplication
+            Case 3                            ' Division
+                If num2 = 0 Then
+                    MessageBox.Show("Cannot divide by zero")
+                    Return
+                End If
+                result = num1 \ num2
+            Case 4 : result = -num1            ' Negation
+            Case 5 : result = num1 * num1      ' Square
+            Case 6 : result = num1 * num1 * num1 ' Cube
+            Case 7 : result = num1 Or num2     ' OR
+            Case 8 : result = num1 And num2    ' AND
+            Case 9 : result = num1 Xor num2    ' XOR
+            Case 10 : result = Not num1        ' NOT
+            Case 11                           ' Modulus
+                If num2 = 0 Then
+                    MessageBox.Show("Cannot divide by zero")
+                    Return
+                End If
+                result = num1 Mod num2
+        End Select
+
+        ' Result
+        lblResult.Text = $"Result: {result}"
+        lblResult.Visible = True
+        lblResult.BringToFront()
+        lblResult.Refresh()
+    End Sub
+
+    Private Sub lblNum1_Click(sender As Object, e As EventArgs) Handles lblNum1.Click
+
+    End Sub
+
+    Private Sub lblNum2_Click(sender As Object, e As EventArgs) Handles lblNum2.Click
+
+    End Sub
+
+    Private Sub txtNum2_TextChanged(sender As Object, e As EventArgs) Handles txtNum2.TextChanged
+
+    End Sub
+End Class
